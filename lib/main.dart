09e013e467437ee9a232c55d52df422a71fd636e
mohamed yasser <medoyasser511@gmail.com>
2025-08-14 16:02:49 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:training_app/features/auth/presentation/views/login.dart';
+
+void main() {
+  // this is the entry point in the app (root)
+  runApp(const NewApp());
+}
+
+class NewApp extends StatelessWidget {
+  const NewApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    );
+  }
+}
