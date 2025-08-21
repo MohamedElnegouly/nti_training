d3bd7afb770748e61e596d1805a49118380476c2
mohamed yasser <medoyasser511@gmail.com>
2025-08-21 18:15:40 +0300

    add exception message

--- lib/core/service/firebase_auth.dart
+++ lib/core/service/firebase_auth.dart
@@ -1,20 +1,25 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:training_app/core/errors/custom_Exception.dart';
 
 class FirebaseAuthService {
-  createUserWithEmailAndPassword({required String emailAddress , required String password})async{
+  Future<User> createUserWithEmailAndPassword(
+      {required String emailAddress, required String password}) async {
     try {
-  final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-    email: emailAddress,
-    password: password,
-  );
-} on FirebaseAuthException catch (e) {
-  if (e.code == 'weak-password') {
-    print('The password provided is too weak.');
-  } else if (e.code == 'email-already-in-use') {
-    print('The account already exists for that email.');
-  }
-} catch (e) {
-  print(e);
-}
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException(message: "كلمة مرور ضعيفه");
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(message: "البريد مستخدم بالفعل");
+      }
+    } catch (e) {
+      throw CustomException(message: "حدث خطأ غير متوقع: $e");
+    }
+    throw CustomException(message: "حدث خطأ غير متوقع أثناء إنشاء المستخدم.");
   }
 }
