afdcb4e5fb8ed7488443baee73222669bcb7e1eb
mohamed yasser <medoyasser511@gmail.com>
2025-08-22 14:58:45 +0300

    repo implement and create signup cubit

--- lib/features/auth/Data/Models/repo/auth_repo_implements.dart
+++ lib/features/auth/Data/Models/repo/auth_repo_implements.dart
@@ -1,12 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:training_app/core/errors/custom_Exception.dart';
+import 'package:training_app/core/errors/failure.dart';
+import 'package:training_app/core/service/firebase_auth.dart';
+import 'package:training_app/features/auth/Data/Models/user_model.dart';
 import 'package:training_app/features/auth/Domin/entities/user_entity.dart';
 import 'package:training_app/features/auth/Domin/repo/auth_repo.dart';
 
-class AuthRepoImplements implements AuthRepo{
+class AuthRepoImplements implements AuthRepo {
   @override
-  Future<Either<String, UserEntity>> createUserWithEmailAndPassword({required String email, required String password}) {
-    // TODO: implement createUserWithEmailAndPassword
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword (
+      {required String email, required String password}) async{
+    try {
+      var firebaseAuthService = FirebaseAuthService();
+      var user = await firebaseAuthService.createUserWithEmailAndPassword(
+          emailAddress: email, password: password);
+      UserModel userModel = UserModel.fromfirebaseUser(user);
+      UserEntity userEntity = userModel.toEntity();
+      return right(userEntity);
+    } on CustomException catch (e) {
+      return left(
+        ServerFailure(message: e.message),
+      );
+    }
   }
-
 }
--- lib/features/auth/Domin/repo/auth_repo.dart
+++ lib/features/auth/Domin/repo/auth_repo.dart
@@ -1,9 +1,10 @@
 
 import 'package:dartz/dartz.dart';
+import 'package:training_app/core/errors/failure.dart';
 import 'package:training_app/features/auth/Domin/entities/user_entity.dart';
 
 abstract class AuthRepo {
   
-Future <Either<String  , UserEntity>>  createUserWithEmailAndPassword(
+Future <Either<Failure  , UserEntity>>  createUserWithEmailAndPassword(
       {required String email, required String password});
 }
